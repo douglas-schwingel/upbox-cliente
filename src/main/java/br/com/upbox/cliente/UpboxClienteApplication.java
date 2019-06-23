ae19cb9e37b7ebb85246ac17e6ee76200bbda5d6
Douglas Schwingel <dgls.sch@gmail.com>
2019-06-23 16:51:57 -0300

    Commit inicial já com as paginas index e cadastro prontas

---
+++ src/main/java/br/com/upbox/cliente/UpboxClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.upbox.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpboxClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpboxClienteApplication.class, args);
+	}
+
+}
